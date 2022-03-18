5f0bb079a7db4f82ea2939e3e05eba930a82424f
diegomarrufo <dmarrufo@ucm.es>
2022-03-18 22:57:35 +0100

    movies slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,85 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { motion } from 'framer-motion'
+import { useState, useEffect, useRef, MutableRefObject } from 'react'
 
 const Home: NextPage = () => {
+  const [width, setWidth] = useState(0)
+  const carrousel = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    // carrousel.current.scrollWidth is the total width of available scrolling
+    // carrousel.current.offsetWidth is the displayed width of the sum of items in the carrousel
+    const currentCarrousel = carrousel.current as HTMLDivElement
+    setWidth(currentCarrousel.scrollWidth - currentCarrousel.offsetWidth)
+  }, [])
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="min-h-screen bg-[#101010] py-2 text-white">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="px-20">
+        <div className="flex h-1/6 items-center justify-between">
+          <nav className="inline-flex">
+            <li>NavItem</li>
+            <li>NavItem</li>
+            <li>NavItem</li>
+            <li>NavItem</li>
+          </nav>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          <div className="inline-flex gap-4">
+            <div>Searchbar</div>
+            <div>Profile</div>
+          </div>
+        </div>
+        <div className="flex h-3/6 items-center">
+          <div>
+            <h1 className="text-bold text-6xl">
+              <span className="text-blue-600">Scene</span> At The Beach
+            </h1>
+            <p>Description about the movie by Kitano</p>
+            <button
+              className="mr-2 rounded-full bg-blue-600 px-20 py-2 text-xl
+            uppercase shadow-lg shadow-blue-800 transition
+            hover:bg-blue-500 hover:shadow-blue-600
+            "
+            >
+              Play
+            </button>
+            <button className="rounded-full border border-white bg-transparent p-2 px-4">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="h-2/6">
+          <h3 className="text-3xl uppercase">My List</h3>
+          <motion.div
+            ref={carrousel}
+            className="carrousel cursor-grab overflow-hidden bg-blue-500"
+            whileTap={{ cursor: 'grabbing' }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <motion.div
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              className="inner-carrousel mr-2 flex gap-4 bg-green-300"
+            >
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+              <motion.div className="item bg-blue-600"></motion.div>
+            </motion.div>
+          </motion.div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
